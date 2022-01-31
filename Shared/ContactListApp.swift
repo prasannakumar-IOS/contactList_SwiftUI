6300179c87ca0d60b319a3042dacced98acf8219
Prasannakumar Manikandan <prasannakumar@mallow-tech.com>
2022-01-31 13:41:27 +0530

    starting commit

---
+++ Shared/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  Shared
+//
+//  Created by Prasannakumar Manikandan on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
